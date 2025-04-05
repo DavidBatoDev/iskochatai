afe57fdd13b3d9f338a78f44b0531de49582fcff
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-06 00:58:18 +0800

    Add PWA support and update configuration
    - Enhanced next.config.ts to include PWA support using next-pwa.
    - Added alias for src directory in webpack configuration.
    - Updated .gitignore to include .env.local.
    - Added manifest.json for PWA configuration.
    - Updated package.json to include new dependencies: lucide-react and next-pwa.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import path from 'path';
+import withPWA from 'next-pwa'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',  // Where the service worker and manifest will be placed
+    register: true,
+    skipWaiting: true,
+  },
+});
+
+
