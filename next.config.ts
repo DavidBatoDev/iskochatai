2016c08d4ee423e7212931148b12c7f4ad8a292e
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-06 15:11:13 +0800

    Remove PWA support and clean up configuration for improved performance

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
-import path from 'path';
-import withPWA from 'next-pwa'
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
 };
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',  // Where the service worker and manifest will be placed
-    register: true,
-    skipWaiting: true,
-  },
-});
-
-
+export default nextConfig;
