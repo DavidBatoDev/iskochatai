159d77670d5e91fbf8fe9508ada57baa500adfa2
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-04-09 00:42:42 +0800

    Add AnimatedGridPattern component and integrate motion effects in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
+"use client";
+
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import FAQSection from "../components/FAQSection";
 import Footer from "../components/Footer";
+import { cn } from "@/lib/utils";
+import { AnimatedGridPattern } from "@/components/magicui/animated-grid-pattern";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-600 to-indigo-800 text-white">
-      <HeroSection />
-      <FAQSection />
-      <Footer />
+    <div className="min-h-screen relative bg-gradient-to-r from-blue-600 to-indigo-800 text-white">
+      <AnimatedGridPattern
+        numSquares={70}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(2000px_circle_at_center,white,transparent)]",
+          "inset-0 h-full w-full absolute"
+        )}
+      />
+      <div className="relative z-10">
+        <HeroSection />
+        <FAQSection />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-indigo-900 bg-opacity-50 py-4 md:py-6 mt-6 md:mt-10">
-      <div className="container mx-auto px-4 text-center text-blue-200 text-xs md:text-sm">
+    <footer className="bg-indigo-900 bg-opacity-50 py-4 md:py-6 mt-6 md:mt-10 z-20">
+      <div className="container mx-auto px-4 text-center text-blue-200 text-xs md:text-sm z-10">
         <p>
           &copy; {new Date().getFullYear()} IskoChat. Helping students achieve
           their academic dreams.
